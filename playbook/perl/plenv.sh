a9d47b4bb12c8eb3ea2896f97810846a9dc807da
kazuya <takizawakazuya@kazuya-no-MacBook-Pro.local>
2014-08-10 13:15:02 +0900

    EditPlenvForZsh

--- playbook/perl/plenv.sh
+++ playbook/perl/plenv.sh
@@ -3,8 +3,8 @@
 
 if [[ ! -e $HOME/.plenv ]];then
    git clone git://github.com/tokuhirom/plenv.git $HOME/.plenv
-   echo 'export PATH="$HOME/.plenv/bin:$PATH"' >> $HOME/.bash_profile
-   echo 'eval "$(plenv init -)"' >> $HOME/.bash_profile
+   echo 'export PATH="$HOME/.plenv/bin:$PATH"' >> $HOME/.zshrc
+   echo 'eval "$(plenv init -)"' >> $HOME/.zshrc
    git clone git://github.com/tokuhirom/Perl-Build.git $HOME/.plenv/plugins/perl-build/
    $HOME/.plenv/bin/plenv install 5.10.1
    $HOME/.plenv/bin/plenv rehash
